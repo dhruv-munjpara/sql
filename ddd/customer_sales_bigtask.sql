9183da4898c2e4cd27f49e3328ff2d5c192d211d
dhruv-munjpara <dhruvmunjpara1511@gmail.com>
2026-01-25 01:05:34 +0530

    added ddd folder

---
+++ ddd/customer_sales_bigtask.sql
@@ -0,0 +1,38 @@
+use bigtask;
+select count(*) from customer_data;
+select count(*) from sales_data;
+select * from customer_data;
+select * from sales_data;
+#Show only invoice_no, customer_id, and price.
+select invoice_no,customer_id,price from sales_data;
+
+#Find all sales where the category is Clothing.
+select * from sales_data where category="Clothing";
+
+#List all unique product categories. in gujrati
+SELECT DISTINCT category FROM sales_data;
+select category from sales_data group by category;
+
+#Show all distinct shopping malls.
+SELECT DISTINCT Shopping_mall FROM sales_data;
+select shopping_mall from sales_data group by shopping_mall;
+
+#Find sales where quantity is greater than 3.
+select * from sales_data where quantity<3;
+
+#Display records where price is greater than 2000.
+select * from sales_data where price<2000;
+
+#Show all sales made in Kanyon mall.
+select * from sales_data where shopping_mall="kanyon";
+
+#Count total number of invoices.
+select count(*) as invoices from sales_data;
+
+#Count total number of customers.
+select count(*) as total_customer from customer_data;
+
+#Display sales ordered by price (highest first).
+SELECT count(*) FROM sales_data ORDER BY CAST(price AS DECIMAL(10,2)) DESC;
+
+
